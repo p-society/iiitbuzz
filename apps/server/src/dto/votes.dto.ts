426f226388478117af0e2ddaa55fb501da0140d9
Hrusi01 <hrusikeshkar01@gmail.com>
2025-12-14 20:09:37 +0530

    updated dto

--- apps/server/src/dto/votes.dto.ts
+++ apps/server/src/dto/votes.dto.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
 export const postIdParamsSchema = z.object({
-	id: z.string().uuid(),
+  postId: z.string().uuid(),
 });
 export const votePayloadSchema = z.object({
-	voteValue: z.literal(1).or(z.literal(-1)).or(z.literal(0)),
+  voteValue: z.union([z.literal(1),z.literal(-1),z.literal(0),
+  ]),
 });
 export type VotePayloadInput = z.infer<typeof votePayloadSchema>;
+export type PostIdParams = z.infer<typeof postIdParamsSchema>;
